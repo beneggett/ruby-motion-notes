d72b10b9e8cf6a95f945d2406d31d430d534141f
Ben Eggett <beneggett@gmail.com>
2015-05-13 19:33:02 -0700

    Ruby tutorial chapter 1

---
+++ HelloMotion/app/app_delegate.rb
@@ -0,0 +1,24 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    rootViewController = UIViewController.alloc.init
+    rootViewController.title = 'HelloMotion'
+    rootViewController.view.backgroundColor = UIColor.whiteColor
+
+    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = navigationController
+    @window.makeKeyAndVisible
+
+    alert = UIAlertView.alloc.initWithTitle("Hello",
+      message:"Hello there cody",
+      delegate:nil,
+      cancelButtonTitle:'Close',
+      otherButtonTitles:nil
+    )
+    alert.show
+    puts "Hello from the console"
+
+    true
+  end
+end
